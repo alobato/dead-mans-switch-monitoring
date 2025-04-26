52b34f962a84a458823e9b77989908083eb03f38
Arley Lobato <arleylobato@gmail.com>
2025-04-26 08:55:33 -0300

    Corrects

--- scripts/collectStatus.sh
+++ scripts/collectStatus.sh
@@ -4,7 +4,7 @@ CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
 MEMORY=$(free | grep Mem | awk '{print ($3/$2)*100}')
 DISK=$(df / | tail -1 | awk '{print $5}' | tr -d '%')
 
-curl -X POST https://seu-hub.com/status/telerison__nome_do_servidor \
+curl -X POST https://seu-hub.com/status/monitoring__nome_do_servidor \
   -H "Authorization: Bearer seu_token_secreto" \
   -H "Content-Type: application/json" \
   -d "{\"cpu\":$CPU,\"memory\":$MEMORY,\"disk\":$DISK}" &>/dev/null
